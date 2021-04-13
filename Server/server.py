6d003e9e3a8f6eaa53d1e748cf305102b888b518
LearnerHB <875352427@qq.com>
2021-04-13 21:39:45 +0800

    Realize user login.

--- Server/server.py
+++ Server/server.py
@@ -31,5 +31,31 @@ def register():
 
 
 
+# 用户登入
+@app.route("/api/project/login", methods=["POST"])
+def login():
+    username = request.form.get("username")
+    password = request.form.get("password")
+    conn = mysql.connector.connect(host="localhost",
+                                   port=3306,
+                                   user="dbuser", password="dbuser666", database="project")  # dbuser password
+    cursor = conn.cursor(dictionary=True)
+
+    query = "SELECT COUNT(*) AS count,user_id AS id FROM user WHERE nickname='"+username+"' and password='"+password+"'"
+    cursor.execute(query)
+    result = cursor.fetchone()
+    print(result)
+    conn.commit()
+    cursor.close()
+    conn.close()
+    if result["count"] == 0:
+        return jsonify(status="error")
+    else:
+        return jsonify(status="ok",id=result["id"])
+
+
+
+
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
