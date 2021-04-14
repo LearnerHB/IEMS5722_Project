67887b60a7035d1748a5c4420d15899424076c0f
LearnerHB <875352427@qq.com>
2021-04-14 12:31:59 +0800

    Reset Client code.

--- Client/app/src/main/java/hk/edu/cuhk/ie/iems5722/a2_1155152636/user/User.java
+++
@@ -1,8 +0,0 @@
-package hk.edu.cuhk.ie.iems5722.a2_1155152636.user;
-
-public class User {
-
-    public static final String name = "CHEN Jiaxian";
-    public static final String id = "1155152636";
-
-}
