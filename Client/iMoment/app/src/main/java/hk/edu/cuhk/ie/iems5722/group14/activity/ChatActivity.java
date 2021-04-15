1cb96777530d9408f92ec4aaf1842db803e0451c
橙夹馅 <chengjiaxian@Lumos.local>
2021-04-15 14:57:00 +0800

    Add Contact: add_friend via userID (accept or refuse) update

---
+++ Client/iMoment/app/src/main/java/hk/edu/cuhk/ie/iems5722/group14/activity/ChatActivity.java
@@ -0,0 +1,19 @@
+package hk.edu.cuhk.ie.iems5722.group14.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.im.R;
+
+public class ChatActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+
+    }
+
+}
