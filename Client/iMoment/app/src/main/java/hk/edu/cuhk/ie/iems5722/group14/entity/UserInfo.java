ea9af4653b57325ffec29629d13a5c434062d79c
橙夹馅 <chengjiaxian@Lumos.local>
2021-04-15 14:38:20 +0800

    Add Register & Login (new)

---
+++ Client/iMoment/app/src/main/java/hk/edu/cuhk/ie/iems5722/group14/entity/UserInfo.java
@@ -0,0 +1,25 @@
+package hk.edu.cuhk.ie.iems5722.group14.entity;
+
+import android.app.Application;
+
+public class UserInfo extends Application{
+
+    private int id;
+    private String nickName;
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNickName(String nickName) {
+        this.nickName = nickName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNickName() {
+        return nickName;
+    }
+}
