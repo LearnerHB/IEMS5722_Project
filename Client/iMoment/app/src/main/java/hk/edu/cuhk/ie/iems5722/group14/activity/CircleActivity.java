85652fee39e1383f0fe732b5d96a063ad25e6429
“Eillotsun” <250853210@qq.com>
2021-04-30 22:43:53 +0800

    refresh circle

--- Client/iMoment/app/src/main/java/hk/edu/cuhk/ie/iems5722/group14/activity/CircleActivity.java
+++ Client/iMoment/app/src/main/java/hk/edu/cuhk/ie/iems5722/group14/activity/CircleActivity.java
@@ -7,7 +7,9 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.util.Log;
 import android.view.View;
+import android.widget.AbsListView;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
@@ -47,9 +49,6 @@ public class CircleActivity extends AppCompatActivity {
 
         try {
             String s = getFriendsRequest.execute(url).get();
-
-
-
             JSONObject jsonObject = new JSONObject(s);
             String status = jsonObject.getString("status");
             if("ok".equals(status)){
@@ -75,6 +74,8 @@ public class CircleActivity extends AppCompatActivity {
         } catch (JSONException e) {
             e.printStackTrace();
         }
+
+
 
         // 新推文
         ImageView new_posts = findViewById(R.id.new_posts);
@@ -102,20 +103,103 @@ public class CircleActivity extends AppCompatActivity {
                             response.show();
                         }
 
-
                     }
                 });
+
                 builder.show();
+                refresh();
+
 
             }
+
+
+
         });
+
+
+
 
         CircleAdapter circleAdapter = new CircleAdapter(CircleActivity.this,R.layout.item_circle,circleMessageList);
         ListView listView = findViewById(R.id.circle);
         listView.setAdapter(circleAdapter);
 
 
+        listView.setOnScrollListener(new AbsListView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(AbsListView view, int scrollState) {
+//                switch (scrollState) {
+//                    case SCROLL_STATE_IDLE:
+//                        boolean toBottom = view.getLastVisiblePosition() == view.getCount() - 1;
+//                        if (toBottom) {
+//                            refresh();
+//                            circleAdapter.notifyDataSetChanged();
+//
+//                        }
+//                        break;
+//                }
+            }
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+                if (firstVisibleItem == 0) {
+                    View firstVisibleItemView = listView.getChildAt(0);
+                    if (firstVisibleItemView != null && firstVisibleItemView.getTop() == 0) {
+                        refresh();
+                        circleAdapter.notifyDataSetChanged();
+
+                        Log.e("ListView", "##### 滚动到顶部 #####");
+                    }
+                } else if ((firstVisibleItem + visibleItemCount) == totalItemCount) {
+                    View lastVisibleItemView = listView.getChildAt(listView.getChildCount() - 1);
+                    if (lastVisibleItemView != null && lastVisibleItemView.getBottom() == listView.getHeight()) {
+                        Log.e("ListView", "##### 滚动到底部 ######");
+                    }
+                }
+            }
+        });
+
+
     }
+
+
+    private void refresh(){
+
+        userInfo = (UserInfo) getApplicationContext();
+        // git test
+        // 获取朋友圈信息
+        String url = "http://18.222.103.240/api/project/posts?user_id="+userInfo.getId();
+        HttpGetTask getFriendsRequest = new HttpGetTask();
+
+        try {
+            String s = getFriendsRequest.execute(url).get();
+            JSONObject jsonObject = new JSONObject(s);
+            String status = jsonObject.getString("status");
+            if("ok".equals(status)){
+
+                JSONArray data = jsonObject.getJSONArray("data");
+                circleMessageList.clear();
+                for(int i=0;i<data.length();i++){
+                    circleMessageList.add(new Circle(data.getJSONObject(i).getString("nickname"),
+                            data.getJSONObject(i).getString("post_content"),
+                            data.getJSONObject(i).getInt("post_id"),
+                            data.getJSONObject(i).getString("is_like"),
+                            data.getJSONObject(i).getInt("likes")));
+                }
+
+            }else {
+                System.out.println("Get data error");
+            }
+
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
 
     private String postRequest(String postContent){
         String URLString = "http://18.222.103.240/api/project/posts";
